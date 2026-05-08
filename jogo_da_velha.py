0fedccf767a5ec7e6d79ff8cd693e0bc87151bbc
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2026-05-07 22:34:37 -0300

    feat: aula 07/05/2026 parte dois

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -50,7 +50,7 @@ def faz_movimento(board, i, j, jogador):
 
 #função para verificar o ganhador
 def verifica_ganhador(board):
-    #estrutura da linha
+    #estrutura da linha 
     for i in range(3):
         if (board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != branco):
             return board[i][0]
@@ -60,7 +60,7 @@ def verifica_ganhador(board):
         if (board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] != branco):
             return board[0][i]
     
-    #estrutura diagonal princial
+    #estrutura diagonal principal
     if (board[0][0] != branco and board[0][0] == board[1][1] and board[1][1] == board[2][2]):
         return board[0][0]
 
